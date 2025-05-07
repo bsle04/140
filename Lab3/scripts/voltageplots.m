f210663b9c424e00aecdd5c447ea5ad127cb37bf
bsle04 <2004brandonle@gmail.com>
2025-05-06 21:38:31 -0700

    part d plots

--- Lab3/scripts/voltageplots.m
+++ Lab3/scripts/voltageplots.m
@@ -1,3 +1,4 @@
+%% part c
 data = readtable("data\RigolDS3.csv");
 time = data.Time_s_;
 ch1 = data.CH1_V_;
@@ -21,5 +22,32 @@ plot(uns_time, uns_ch1, 'b-', uns_time, uns_ch2, 'r--');
 xlabel('Time (s)');
 ylabel('Voltage (V)');
 title('Noninverting Amplifier Unsaturated Plot');
+legend('Vin', 'Vout');
+grid on;
+
+%%part d
+data = readtable("data\RigolDS4.csv");
+time = data.Time_s_;
+ch1 = data.CH1_V_;
+ch2 = data.CH2_V_;
+
+figure;
+plot(time, ch1, 'b-', time, ch2, 'r--');
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Inverting Amplifier');
+legend('Vin', 'Vout');
+grid on;
+
+data = readtable("data\RigolDS5.csv");
+time = data.Time_s_;
+ch1 = data.CH1_V_;
+ch2 = data.CH2_V_;
+
+figure;
+plot(time, ch1, 'b-', time, ch2, 'r--');
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Inverting Amplifier');
 legend('Vin', 'Vout');
 grid on;
